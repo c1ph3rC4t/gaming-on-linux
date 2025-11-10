6486481b28cf556e33c0f3f2d339db01b43a9527
c1ph3rC4t <c1ph3rC4t@gmail.com>
2025-11-10 20:19:36 +0100

    Create maccel-install.sh for installation and permissions
    This script installs maccel and resolves permission issues.

---
+++ maccel-install.sh
@@ -0,0 +1,9 @@
+###################################################
+## Created this script because maccel was        ##
+## broken on my device due to permission issues. ##
+###################################################
+
+curl -fsSL https://www.maccel.org/install.sh | sudo sh
+sudo usermod -aG maccel $USER
+sudo chmod 660 /dev/maccel
+sudo chmod 666 /sys/module/maccel/parameters/*
