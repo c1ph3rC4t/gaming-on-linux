4f802aa6d435a8d9db785aae9b7da2cd9b04ce5b
c1ph3rC4t <c1ph3rC4t@gmail.com>
2025-11-10 19:35:36 +0100

    Add uninstall script for osu!lazer on Linux
    This script removes osu!lazer binaries and related files.

---
+++ osu-uninstall.sh
@@ -0,0 +1,8 @@
+######################################################
+## A simple script to uninstall osu!lazer on Linux. ##
+## I created this to complement my install script.  ##
+######################################################
+
+sudo rm -f /usr/local/bin/osu
+sudo rm -f /usr/share/pixmaps/osu.png
+sudo rm -f /usr/share/applications/osu.desktop
