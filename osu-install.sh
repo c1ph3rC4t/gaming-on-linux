f8f79777c375bcbb3f4886ffa6a5319f4ad85608
c1ph3rC4t <c1ph3rC4t@gmail.com>
2025-11-10 19:33:14 +0100

    Add osu!lazer installation script for Linux
    This script downloads and installs osu!lazer on Linux, including the desktop entry.

---
+++ osu-install.sh
@@ -0,0 +1,10 @@
+####################################################
+## A simple script to install osu!lazer on Linux. ##
+## I created this because the AUR version of      ##
+## osu!lazer kept insisting it was out of date!   ##
+####################################################
+
+sudo wget https://github.com/ppy/osu/releases/latest/download/osu.AppImage -O /usr/local/bin/osu
+sudo chmod +x /usr/local/bin/osu
+sudo wget https://raw.githubusercontent.com/ppy/osu/master/assets/lazer.png -O /usr/share/pixmaps/osu.png
+echo '[Desktop Entry]\nType=Application\nName=osu!lazer\nIcon=/usr/share/pixmaps/osu.png\nComment=Open source free-to-win rhythm game\nExec=/usr/local/bin/osu %U\nCategories=Game;\nTerminal=false\nStartupWMClass=osu!\n' | sudo tee /usr/share/applications/osu.desktop > /dev/null
