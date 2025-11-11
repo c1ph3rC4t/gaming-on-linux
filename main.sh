745238a323781ec6fdb32b4fa83c9632c8006e18
c1ph3rc4t <c1ph3rc4t@gmail.com>
2025-11-11 21:30:14 +0100

    Added main install script

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -s "https://api.github.com/repos/c1ph3rc4t/gaming-on-linux/contents/scripts" | grep '"name":.*\.sh"' | cut -d'"' -f4 | nl
+
+read -p "Enter number: " CHOICE
+
+SCRIPT=$(curl -s "https://api.github.com/repos/c1ph3rc4t/gaming-on-linux/contents/scripts" | grep '"name":.*\.sh"' | cut -d'"' -f4 | sed -n "${CHOICE}p")
+
+curl -fsSL "https://c1ph3rc4t.github.io/gaming-on-linux/scripts/$SCRIPT" | sh
