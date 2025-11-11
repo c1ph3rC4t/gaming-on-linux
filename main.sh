021b1d82b0066895f73c3a8a86eb19dc0e354450
c1ph3rc4t <c1ph3rc4t@gmail.com>
2025-11-11 21:33:16 +0100

    Fixed main.sh

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 
 curl -s "https://api.github.com/repos/c1ph3rc4t/gaming-on-linux/contents/scripts" | grep '"name":.*\.sh"' | cut -d'"' -f4 | nl
 
-read -p "Enter number: " CHOICE
+read -p "Enter number: " CHOICE </dev/tty
 
 SCRIPT=$(curl -s "https://api.github.com/repos/c1ph3rc4t/gaming-on-linux/contents/scripts" | grep '"name":.*\.sh"' | cut -d'"' -f4 | sed -n "${CHOICE}p")
 
